2ddc3cda59bde6abbec1492f2e93923974ac61d3
Noelia Malisia <noeliamalisia@gmail.com>
2025-04-28 21:08:22 -0300

    strCmp

--- src/4 - Memoria dinámica/Memoria.asm
+++ src/4 - Memoria dinámica/Memoria.asm
@@ -14,7 +14,7 @@ global strLen
 
 ; ** String **
 
-; int32_t strCmp(char* a, char* b)
+; 
 ; a->RDI, b->RSI
 ; 0 si son iguales
 ; 1 si a < b
@@ -27,19 +27,19 @@ strCmp:
 	mov BL, byte [RSI] ;primer caracter de b
 	
 .loop:
-	mov AL, byte [RDI]   ; <-- Cargar SIEMPRE en cada vuelta
-    mov BL, byte [RSI]
-	cmp AL, '\0'
-	je .fin
-	cmp BL, '\0'
-	je .fin
-	cmp AL, BL
-	je .equal
+	;mov AL, byte [RDI]   
+    ;mov BL, byte [RSI]
+	cmp AL, 0 ;chequeo si es el caracter nulo
+	je .check ;si es el caracter nulo ya recorri toda la cadena, chequeo si la otra cadena es mas larga o no
+	cmp BL, 0 ;chequeo si es el caracter nulo
+	je .check ;si es el caracter nulo ya recorri toda la cadena
+	;si estoy aca son dos caracteres no nulos
+	cmp AL, BL 
 	;no son iguales
 	jl .a_is_smaller
 	jg .a_is_greater
-
-
+	je .equal 
+	
 .a_is_smaller:
 	mov EAX, 1
 	jmp .return
@@ -48,9 +48,8 @@ strCmp:
 	mov EAX, -1
 	jmp .return
 	
-	
 .equal:
-	;no son el caracter nulo, tengo q comparar el resto del str
+	;tengo q comparar el resto del str
 	inc RDI
 	inc RSI
 	mov AL, byte [RDI]
@@ -62,19 +61,29 @@ strCmp:
 	mov EAX, 0 ;son el mismo string, devuelvo 0
 	jmp .return
 
-.fin:
-	cmp AL, BL
-	je .same_str
-	cmp AL, '\0'
-	je .a_is_smaller
-	jmp .a_is_greater
+.check:
+	;si estoy aca es pq AL o BL son '\0'
+	cmp AL, BL ; chequeo si los dos son el caracter nulo
+	je .same_str ;en ese caso las dos cadenas se terminan, son iguales
+	cmp AL, 0 ; no se si es necesario
+	je .a_is_smaller ;la cadena 1 es mas corta, por ende la mas chica
+	jmp .a_is_greater ;la cadena 2 es mas corta
 
 .return:
 	pop RBP
 	ret
-
+;Genera una copia del string pasado por parámetro. El puntero pasado siempre es válido
+;aunque podría corresponderse a la cadena vacía.
 ; char* strClone(char* a)
 strClone:
+	push RBP
+	mov RBP, RSP
+	
+
+
+
+
+	pop RBP
 	ret
 
 ; void strDelete(char* a)
--- src/4 - Memoria dinámica/main.c
+++ src/4 - Memoria dinámica/main.c
@@ -6,8 +6,9 @@
 
 #include "../test-utils.h"
 #include "Memoria.h"
-
+int32_t strCmp(char* a, char* b); 
 int main() {
 	/* Acá pueden realizar sus propias pruebas */
+	printf("%d\n", strCmp("sar", "23"));   // debe dar -1
 	return 0;
 }
